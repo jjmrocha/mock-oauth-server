51ea50b3aad835017a9f3b4fffb8395c8c4cafaf
Joaquim Rocha <jrocha.github@gmailbox.org>
2022-03-29 09:35:19 +0100

    Project init (#1)
    Co-authored-by: Joaquim Rocha <joaquim.rocha@daimler.com>

---
+++ lib/build.gradle.kts
@@ -0,0 +1,64 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    `java-library`
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+group = "net.uiqui"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // kotlin
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    //
+    // testing
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("io.mockk:mockk:1.10.6")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events(
+            FAILED,
+            STANDARD_ERROR,
+            SKIPPED,
+            PASSED
+        )
+        exceptionFormat = FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
+}
