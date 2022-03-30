0d7d044e0e0b855c33ea6caab0eb71c76934099e
Joaquim Rocha <jrocha.github@gmailbox.org>
2022-03-30 22:27:01 +0100

    Add support for JWT generation (#3)
    Co-authored-by: Joaquim Rocha <joaquim.rocha@daimler.com>

---
+++ lib/src/test/kotlin/net/uiqui/oauth/mock/JWTGeneratorTest.kt
@@ -0,0 +1,45 @@
+package net.uiqui.oauth.mock
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class JWTGeneratorTest {
+    @Test
+    fun `test jwt generation`() {
+        // given
+        val classUnderTest = JWTGenerator()
+        val claims = mapOf(
+            "aud" to "audience",
+            "iss" to "https://issuer/",
+            "custom-float" to 123.45,
+            "custom-list" to listOf("value1", "value2")
+        )
+        // when - encoded
+        val jwt = classUnderTest.generate(claims)
+        // then
+        assertThat(jwt).isInstanceOf(String::class.java)
+        // when - decoded
+        val result = classUnderTest.parseJwt(jwt)
+        // then
+        assertThat(result["aud"] as List<*>).contains("audience")
+        assertThat(result["iss"]).isEqualTo("https://issuer/")
+        assertThat(result["custom-float"]).isEqualTo(123.45)
+        assertThat(result["custom-list"] as List<*>).containsExactly("value1", "value2")
+    }
+
+    @Test
+    fun `test JWKS creation`() {
+        // given
+        val classUnderTest = JWTGenerator()
+        // when
+        val result = classUnderTest.getJWKS()
+        // then
+        assertThat(result.keys).hasSize(1)
+        val jwk = result.keys.first()
+        assertThat(jwk.kty).isEqualTo("RSA")
+        assertThat(jwk.e).isEqualTo("AQAB")
+        assertThat(jwk.use).isEqualTo("sig")
+        assertThat(jwk.kid).matches("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}")
+        assertThat(jwk.n).isNotNull
+    }
+}
